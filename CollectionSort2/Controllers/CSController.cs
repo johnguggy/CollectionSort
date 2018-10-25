ce613ec73709e8c8dc8c16a88291634f1d18f9ba
johnguggy <42472160+johnguggy@users.noreply.github.com>
2018-10-25 14:57:38 -0400

    Add files via upload

---
+++ CollectionSort2/Controllers/CSController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollectionSort2.Controllers
+{
+    public class CSController : Controller
+    {
+        // GET: CS
+        public ActionResult Index()
+        {
+            Random rand = new Random();
+            List<int> rtnlist = new List<int>();
+
+            for (int i = 1; i < 5; i++)
+            {
+                rtnlist.Add(rand.Next(100));
+            }
+            rtnlist.Sort();
+            ViewBag.randnums = rtnlist;
+            return View();
+        }
+    }
+}
